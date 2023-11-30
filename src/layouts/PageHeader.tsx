f1e918675631c9f8c69f9845d8c8c63a63915494
gjuliao <gjuliao32@gmail.com>
2023-11-30 14:54:03 -0500

    Added page header with icons

---
+++ src/layouts/PageHeader.tsx
@@ -0,0 +1,17 @@
+import { Menu } from 'lucide-react'
+import React from 'react'
+
+export const PageHeader = () => {
+  return (
+    <div className='flex gap-10 lg:gap-20 justify-between'>
+        <div className='flex gap-2 items-center flex-shrink-0'>
+        <Menu />
+            <a href='/'>
+                <h1 className='text-2xl font-bold text-red-500 hover:text-red-700'>
+                    YouTube
+                </h1>
+            </a>
+        </div>
+    </div>
+  )
+}
