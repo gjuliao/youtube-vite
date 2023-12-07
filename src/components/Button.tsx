e4668931a4499666f3d03e20273c054217044425
gjuliao <gjuliao32@gmail.com>
2023-12-07 09:11:53 -0500

    Added styles to button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,29 @@
-import { cva } from "class-variance-authority"
+import { VariantProps, cva } from "class-variance-authority"
+import { ComponentProps } from "react"
 
-export const Button = () => {
+const buttonStyles = cva(['transition-colors'], {
+  variants: {
+    variant: {
+      default: ["bg-secondary", "hover:bg-secondary-hover"],
+      ghost: ["hover:bg-gray-100"]
+    },
+    size: {
+        default: ["rounded", "p-2"],
+        icon: [ "rounded-full", "w-10", "h-10", "flex", "items-center", "justify-center", "p-2.5"],
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default"
+    },
+  },
+})
 
-  const buttonStyles = cva(["hover:bg-secondary-hover", 'transition-colors'])
+type ButtonProps = VariantProps<typeof buttonStyles> &
+ComponentProps<"button">
+
+export const Button = ({variant, size, ...props }: ButtonProps) => {
+
   return (
-    <button />
+    <button {...props}  className={buttonStyles({variant, size})}/>
   )
 }
